c1bc76dafcb33aff9e5eaaeb21e3bfdbbae37024
joko751214 <47811731+joko751214@users.noreply.github.com>
2020-12-01 17:49:49 +0800

    adjust aos animate

---
+++ app/assets/js/all.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//   $('.product-menu-list').on('click', function(e) {
+//     e.preventDefault();
+//     console.log(this);
+//     $('.product-menu-list a').toggleClass('active');
+//   });
+// });
