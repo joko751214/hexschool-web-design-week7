e14b8a36e08547ddfa7148904ae300c3554cfc5b
joko751214 <joko751214@gmail.com>
2020-12-01 21:50:00 +0800

    week7 finish

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,7 +1,21 @@
-// $(document).ready(function() {
-//   $('.product-menu-list').on('click', function(e) {
-//     e.preventDefault();
-//     console.log(this);
-//     $('.product-menu-list a').toggleClass('active');
-//   });
-// });
+$(document).ready(function() {
+  $(window).scroll(function() {
+    const scrolled = $(this).scrollTop();
+    const windowHeight = $(window).height();
+    const gearBg = $('.gear-bg').offset().top;
+    const strippedBg = $('.stripped-bg').offset().top;
+    const jumbotron = $('.jumbotron').offset().top;
+
+    if (scrolled + windowHeight >= gearBg) {
+      $('.gear-bg').css("background-position",`-${parseInt($(this).scrollTop()*0.1)}px center`);
+    }
+
+    if (scrolled + windowHeight >= strippedBg) {
+      $('.stripped-bg').css("background-position",`-${parseInt($(this).scrollTop()*0.1)}px center`);
+    }
+
+    // if (scrolled + windowHeight >= jumbotron) {
+    //   $('.jumbotron').css("background-position",`center -${parseInt($(this).scrollTop()*0.2)}px`);
+    // }
+  })
+});
